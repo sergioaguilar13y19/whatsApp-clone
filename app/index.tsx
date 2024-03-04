26490d6994b4903bbe04fc3ab4053a815a508f44
sergioaguilar13y19 <sergioaguilar13y19@gmail.com>
2024-03-04 12:15:11 -0600

    Añadir soporte para enrutamiento y localización en la aplicación

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Image } from "react-native";
+import React from "react";
+import Colors from "@/constants/Colors";
+const welcome_image = require('@/assets/images/welcome.png')
 
 const Page = () => {
   return (
-    <View>
-      <Text>Page</Text>
+    <View style={styles.container}>
+      <Image source={welcome_image} style={styles.welcome} />
+      <Text>Bienvenido </Text>
     </View>
-  )
-}
+  );
+};
 
-export default Page
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  welcome: {
+    height: 400,
+    aspectRatio: 1,
+    marginBottom: 80,
+  },
+  headline:{
+
+  },
+  description:{},
+  link:{
+    color: Colors.gray,
+  },
+})
+
+export default Page;
